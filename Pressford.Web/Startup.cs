ba1c45b7b55901ecd447bdae34359452cede1034
ricardo abreu <rj.abreu@outlook.com>
2016-09-26 08:26:54 +0100

    clean up

--- Pressford.Web/Startup.cs
+++ Pressford.Web/Startup.cs
@@ -15,7 +15,7 @@ namespace Pressford.Web
             createRolesandUsers();
         }
 
-        // In this method we will create default User roles and Admin user for login   
+         
         private void createRolesandUsers()
         {
             ApplicationDbContext context = new ApplicationDbContext();
@@ -23,17 +23,15 @@ namespace Pressford.Web
             var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
             var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
 
-
-            // In Startup iam creating first Admin Role and creating a default Admin User    
+ 
             if (!roleManager.RoleExists("Admin"))
             {
 
-                // first we create Admin rool   
+                  
                 var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
                 role.Name = "Admin";
                 roleManager.Create(role);
-
-                //Here we create a Admin super user who will maintain the website                  
+             
 
                 var user = new ApplicationUser();
                 user.UserName = "admin@pressford.com";
@@ -43,7 +41,7 @@ namespace Pressford.Web
 
                 var chkUser = UserManager.Create(user, userPWD);
 
-                //Add default User to Role Admin   
+              
                 if (chkUser.Succeeded)
                 {
                     var result1 = UserManager.AddToRole(user.Id, "Admin");
@@ -53,7 +51,7 @@ namespace Pressford.Web
 
             
 
-            // creating Creating Employee role    
+           
             if (!roleManager.RoleExists("Employee"))
             {
                 var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
